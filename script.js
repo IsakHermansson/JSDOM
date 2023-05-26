576aac8f83c534eb93ed17a86446de8fe95d0eee
IsakHermansson <92087106+IsakHermansson@users.noreply.github.com>
2023-05-26 14:43:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function bgcolor(){
+    document.querySelector("body").style.backgroundColor = "royalblue"
+}
+
+
+function changeImg(){
+    document.getElementById('pic').src = 'dice-6.png'; 
+}
+
+function goodvibes(){
+    let btn2 = document.createElement("p")
+    btn2.innerHTML = "you are amazing";
+    document.body.appendChild(btn2);
+}
+
+
+
+
+
+
